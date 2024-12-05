8004c28794f7a4c906ef84f8b436627e8269c357
josmardiaz92 <josmardiaz92@gmail.com>
2024-12-05 09:42:17 -0400

    bases del inicio

--- www/js/index.js
+++ www/js/index.js
@@ -3,49 +3,128 @@ const contenedorApp=document.getElementById('body');
 const animales=[
     {
         tipo: 'vaca',
-        peso: '80 kg',
-        edad: '5 años'
+        peso: 80,
+        unidadMedida: 'kg',
+        edad: 5,
+        meses: 0
     },
     {
         tipo: 'perro',
-        peso: '10 kg',
-        edad: '3 años'
+        peso: 10,
+        unidadMedida: 'kg',
+        edad: 3,
+        meses: 0
     },
     {
         tipo: 'gato',
-        peso: '5 kg',
-        edad: '2 años'
+        peso: 5,
+        unidadMedida: 'kg',
+        edad: 2,
+        meses: 0
+    },
+    {
+        tipo: 'vaca',
+        peso: 70,
+        unidadMedida: 'kg',
+        edad: 4,
+        meses: 0
     }
-]
+];
+const insumos=[
+    {
+        tipo: 'alimento de vaca',
+        peso: 10,
+        unidadMedida: 'kg',
+        cantidad: 2,
+    },
+    {
+        tipo: 'gatarina',
+        peso: 1,
+        unidadMedida: 'kg',
+        cantidad: 5,
+    },
+    {
+        tipo: 'alimento de perro',
+        peso: 5,
+        unidadMedida: 'kg',
+        cantidad: 3,
+    }
+];
+
 
 function mostrarInicio(){
-    contenedorApp.innerHTML='';
-    const contenedorAnimales=document.createElement('div');
-    contenedorAnimales.classList.add('row');
-    contenedorAnimales.innerHTML=`
-        <div class="col-12">
-            <h1>Animales</h1>
+    const resumenAnimales={};
+    const resumenInsumos={};
+    const resultadoAnimales=filtrar(animales,resumenAnimales);
+    const resultadoInsumos=filtrar(insumos,resumenInsumos);
+
+
+    contenedorApp.innerHTML=`
+        <div class="row">
+            <div class="col-12">
+                <h1>Animales</h1>
+            </div>
+            <div class="col-12 d-flex justify-content-center">
+                <table class="text-center">
+                    <tr>
+                        <th>Animal</th>
+                        <th>Cantidad</th>
+                    </tr>
+                    ${Object.entries(resultadoAnimales).map(([tipo, cantidad]) => `
+                        <tr>
+                            <td>${tipo}</td>
+                            <td>${cantidad}</td>
+                        </tr>
+                    `).join('')}                    
+                    
+                    <tr>
+                        <td colspan="3"><b>Total de animales:</b></td>
+                        <td><b>${Object.values(resultadoAnimales).reduce((a, b) => a + b, 0)}</b></td>
+                    </tr>
+                </table>
+            </div>
         </div>
-        <div class="col-12 d-flex justify-content-center">
-        <table id="tablaAnimales">
-            <tr>
-                <th>Tipo</th>
-                <th>Edad (años)</th>
-                <th>Peso (kg)</th>
-            </tr>
-        </table>
-    `;
-    contenedorApp.appendChild(contenedorAnimales);
-    const tablaAnimales=document.getElementById('tablaAnimales');
-    animales.forEach(animal=>{
-        tablaAnimales.innerHTML+=`
-            <tr>
-                <td>${animal.tipo}</td>
-                <td>${animal.edad}</td>
-                <td>${animal.peso}</td>
-            </tr>
-        `
-    })
+        <div class="row">
+            <div class="col-12">
+                <h1>Insumos</h1>
+            </div>
+            <div class="col-12 d-flex justify-content-center">
+                <table class="text-center">
+                    <tr>
+                        <th>Insumo</th>
+                        <th>peso unitario</th>
+                        <th>peso total</th>
+                    </tr>
+                    ${insumos.map(insumo=>{
+                        const pesoTotal=insumo.peso*insumo.cantidad;
+                        return `
+                            <tr>
+                                <td>${insumo.tipo}</td>
+                                <td>${insumo.peso} ${insumo.unidadMedida}</td>
+                                <td>${pesoTotal} ${insumo.unidadMedida}</td>
+                        </tr>`
+                    })}   
+                    <tr>
+                        <td colspan="3"><b>Total de Insumos:</b></td>
+                        <td><b>${Object.values(resultadoInsumos).reduce((a, b) => a + b, 0)}</b></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    `
 };
 
+function filtrar(objeto,resultado){
+
+    objeto.forEach(element => {
+        if (resultado[element.tipo]) {
+            resultado[element.tipo]++;
+        } else {
+            resultado[element.tipo] = 1;
+        }
+    });
+    return resultado;
+}
+
 mostrarInicio();
+
